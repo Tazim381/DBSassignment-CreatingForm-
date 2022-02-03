00a56f25f7e1f1b31d9add0ec3a975714c6c3364
Tazim <70653485+Tazim381@users.noreply.github.com>
2022-02-04 02:44:05 +0600

    Add files via upload
    Create three form and connect those to database

---
+++ dbsassignment/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <link rel="stylesheet" type ="text/css" href="index.css">
+</head>
+
+<body>
+
+<h1>Click One of three options</h1>
+
+<h3>Insert Movie Table </h3>
+<button class="btn btn-primary" onclick="window.location.href='movie.php'; " >Movie Table </button>
+<h3 >Insert Person Table </h3>
+<button class="btn btn-primary" onclick="window.location.href='person.php';"> Person Table </button>
+
+<h3 >Insert Role Table </h3>
+
+<button class="btn btn-primary" onclick="window.location.href='role.php';"> Role Table</button>
+</body>
+
+</html>
